3e9713f9e165050d88504226fac003314cc99ce0
dgabin <gabindepaire@imac-de-dgabin.home>
2023-03-02 15:26:33 +0100

    Add: formatRows datetime

--- src/Service/ProductsFormatter.php
+++ src/Service/ProductsFormatter.php
@@ -12,15 +12,12 @@ class ProductsFormatter
         $data = [];
 
         foreach($array as $values) {
-            $datetime = explode(" ", $values['created_at']);
-            $date = date_create($datetime[0]);
-
             $row = [
                 'Sku' => $values['sku'],
                 'Status' => $values['is_enabled'] ? 'Enable' : 'Disable',
                 'Price' => str_replace(".", ",", $values['price']) . $values['currency'],
                 'Description'=> preg_replace("/(<br\W*?\/>)|\\\\r/", "\n", $values['description']),
-                'Created At' => date_format($date, 'l, d-M-Y') . ' ' . $datetime[1] . ' ' . $date->format('e'),
+                'Created At' => (new \Datetime($values['created_at']))->format('l, d-M-Y H:i:s'),
                 'Slug' =>strtolower(preg_replace('/\s+/', '_', preg_replace('/[^A-Za-z0-9 ]/', '-', str_replace(",", "", $values['title']) )))
             ];
             $data[] = $row;
