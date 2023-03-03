bb56a1a7b7c001352067d8180175b03ede80b537
dgabin <gabindepaire@imac-de-dgabin.home>
2023-03-03 13:48:33 +0100

    Add: formatRows datetimezone

--- src/Service/ProductsFormatter.php
+++ src/Service/ProductsFormatter.php
@@ -17,7 +17,7 @@ class ProductsFormatter
                 'Status' => $values['is_enabled'] ? 'Enable' : 'Disable',
                 'Price' => str_replace(".", ",", $values['price']) . $values['currency'],
                 'Description'=> preg_replace("/(<br\W*?\/>)|\\\\r/", "\n", $values['description']),
-                'Created At' => (new \Datetime($values['created_at']))->format('l, d-M-Y H:i:s'),
+                'Created At' => (new \Datetime($values['created_at'], new \DateTimeZone('CET')))->format('l, d-M-Y H:i:s e'),
                 'Slug' =>strtolower(preg_replace('/\s+/', '_', preg_replace('/[^A-Za-z0-9 ]/', '-', str_replace(",", "", $values['title']) )))
             ];
             $data[] = $row;
